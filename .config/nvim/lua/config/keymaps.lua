604f972018a3c206f5768a541fbd4fd792b304c2
Randolph <randopledesma+gitlab@hotmail.com>
2026-03-25 12:45:24 +0000

    Enhancements on neovim lazyvim configuration

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Open netrw in current working directory
+vim.keymap.set("n", "<leader>we", function()
+  vim.cmd("Explore " .. vim.fn.getcwd())
+end, { desc = "Explorer (netrw) in CWD" })
